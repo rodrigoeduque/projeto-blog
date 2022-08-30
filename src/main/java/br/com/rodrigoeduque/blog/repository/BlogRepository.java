a4f63b301813592b5559e4a5d45ed5381c62cc13
Rodrigo Duque <rodrigo@DESKTOP-KV6R13N.localdomain>
2022-08-30 02:36:06 -0300

    criado model e repository

---
+++ src/main/java/br/com/rodrigoeduque/blog/repository/BlogRepository.java
@@ -0,0 +1,9 @@
+package br.com.rodrigoeduque.blog.repository;
+
+import br.com.rodrigoeduque.blog.model.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BlogRepository extends JpaRepository<Post,Long> {
+}
