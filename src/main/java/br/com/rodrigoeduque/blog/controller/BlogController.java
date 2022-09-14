2035ee0a8b202ad2d4864ad1ab67e46b534cf548
Rodrigo Duque <rodrigoeduque@hotmail.com>
2022-09-14 15:10:29 -0300

    renderizando post detail

--- src/main/java/br/com/rodrigoeduque/blog/controller/BlogController.java
+++ src/main/java/br/com/rodrigoeduque/blog/controller/BlogController.java
@@ -5,6 +5,7 @@ import br.com.rodrigoeduque.blog.service.BlogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -20,6 +21,14 @@ public class BlogController {
         ModelAndView mv = new ModelAndView("posts");
         List<Post> posts = blogService.findAll();
         mv.addObject("posts", posts);
+        return mv;
+    }
+
+    @GetMapping("/posts/{id}")
+    public ModelAndView getPostDetails(@PathVariable Long id) {
+        ModelAndView mv = new ModelAndView("postDetail");
+        Post post = blogService.findById(id);
+        mv.addObject("post", post);
         return mv;
     }
 
