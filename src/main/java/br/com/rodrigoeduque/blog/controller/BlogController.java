d4ebb9fddaf933ef737a7541a98c56f2982c948a
Rodrigo Duque <rodrigoeduque@hotmail.com>
2022-09-16 17:24:26 -0300

    criado template form e redirecionamentos para inserção de novos posts

--- src/main/java/br/com/rodrigoeduque/blog/controller/BlogController.java
+++ src/main/java/br/com/rodrigoeduque/blog/controller/BlogController.java
@@ -4,10 +4,15 @@ import br.com.rodrigoeduque.blog.model.Post;
 import br.com.rodrigoeduque.blog.service.BlogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
+import java.time.LocalDate;
 import java.util.List;
 
 @Controller
@@ -30,6 +35,22 @@ public class BlogController {
         Post post = blogService.findById(id);
         mv.addObject("post", post);
         return mv;
+    }
+
+    @GetMapping("/newpost")
+    public String getPostForm() {
+        return "postForm";
+    }
+
+    @PostMapping("/newpost")
+    public String savePost(@Valid Post post, BindingResult result, RedirectAttributes attributesS){
+
+        if (result.hasErrors()){
+            return "redirect:/newpost";
+        }
+        post.setDataCriacao(LocalDate.now());
+        blogService.save(post);
+        return "redirect:/posts";
     }
 
 }
